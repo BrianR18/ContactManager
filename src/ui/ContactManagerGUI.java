0530a619a3a69abb8a653a27c71301cf52bb4937
Brian Romero <briomero76@gmail.com>
2021-02-08 16:21:31 -0500

    Cambios pequeños

--- src/ui/ContactManagerGUI.java
+++ src/ui/ContactManagerGUI.java
@@ -1,29 +1,40 @@
 package ui;
 
-import model.Contact;
-import java.util.ArrayList;
+//import model.Contact;
+//import java.util.ArrayList;
 import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 
 public class ContactManagerGUI {
-	private ArrayList<Contact> contacts;
+	//private ArrayList<Contact> contacts;
 	@FXML private Pane mainPane;
+	@FXML private TextField tfName;
+	@FXML private TextField tfEmail;
 	
 	public ContactManagerGUI() {
-		contacts = new ArrayList<Contact>();
+		//contacts = new ArrayList<Contact>();
 	}//End constructor.
 	@FXML
 	public void showSceneAddContact(ActionEvent event) throws IOException{
-		Parent addContactScene = FXMLLoader.load(getClass().getResource("addContactWindow.fxml"));
+		FXMLLoader fxml = new FXMLLoader(getClass().getResource("addContactWindow.fxml"));
+		fxml.setController(this);
+		Parent addContactScene = fxml.load();
 		mainPane.getChildren().setAll(addContactScene);
 	}//End changeSceneAddContact
 	@FXML
 	public void showSceneContactList(ActionEvent event) throws IOException{
-		Parent addContactScene = FXMLLoader.load(getClass().getResource("ContactListWindow.fxml"));
-		mainPane.getChildren().setAll(addContactScene);
+		FXMLLoader fxml = new FXMLLoader(getClass().getResource("ContactListWindow.fxml"));
+		fxml.setController(this);
+		Parent contactListScene = fxml.load();
+		mainPane.getChildren().setAll(contactListScene);
 	}//End changeSceneAddContact
+	@FXML
+	public void saveContact(){
+		
+	}//End saveContact
 }
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -7,13 +7,19 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application{
+	private ContactManagerGUI CMG;
 	
+	public Main(){
+		CMG = new ContactManagerGUI();
+	}//End constructor
 	public static void main(String[] args) {
 		launch(args);
 	}//End main
 	@Override
 	public void start(Stage window) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
+		FXMLLoader fxml = new FXMLLoader(getClass().getResource("MainWindow.fxml"));
+		fxml.setController(CMG);
+		Parent root = fxml.load();
 		Scene scene = new Scene(root,null);
 		window.setTitle("Contact Manager");
 		window.setScene(scene);
